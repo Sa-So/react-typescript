f42fc00685c67a4f20f75017cb8f0b9cdc3773f6
Sahil Soni <60461699+Sa-So@users.noreply.github.com>
2022-05-30 07:52:38 -0400

    Update and rename convert-time.ts to convert-time.js

--- Utilities/convert-time.js
+++ Utilities/convert-time.js
@@ -15,6 +15,25 @@ const convertTo12HourFormat = (time) => {
   }
   return `${newHours}:${newMinutes} ${ampm}`;
 };
+const convertTo24HourFormat = (time) => {
+  const timeArray = time.split(':');
+  const hours = Number(timeArray[0]);
+  const minutes = timeArray[1].split(' ')[0];
+  const seconds = '00';
+  if(time.includes('PM')) {
+      if(hours==12){
+          return `${hours}:${minutes}:${seconds}`;
+      }
+    return `${(hours + 12) % 24}:${minutes}:${seconds}`;
+  }
+  else{
+      if(hours==12){
+          return `${(hours + 12) % 24}:${minutes}:${seconds}`;
+      }
+      return `${hours}:${minutes}:${seconds}`;
+  }
+};
+console.log(convertTo24HourFormat('12:00 PM'));
 console.log(convertTo12HourFormat("23:59:00"));
 console.log(convertTo12HourFormat("12:00:00"));
 console.log(convertTo12HourFormat("00:00:00"));
